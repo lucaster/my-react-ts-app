b0858b8f72d89afc070508706c835c34eca049c5
Luca <luca.cavagnoli@gmail.com>
2021-10-16 21:35:36 +0200

    nested routes

--- src/pages/welcome/WelcomeComponent.tsx
+++ src/pages/welcome/WelcomeComponent.tsx
@@ -1,6 +1,18 @@
+import { Route, useLocation } from "react-router-dom";
+
 const WelcomeComponent = () => {
+  const loc = useLocation<string>();
   return (
-    <h1>The Welcome Page</h1>
+    <section>
+      <h1>The Welcome Page</h1>
+      {/*
+        bisogna x forza mettere full path, non è riusabile :/
+        Workaround: https://stackoverflow.com/a/49321289
+      */}
+      <Route path="/welcome/new-user">
+        <p>Welcome new user!</p>
+      </Route>
+    </section>
   );
 };
 
