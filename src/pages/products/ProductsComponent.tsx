5773cd51ef06369fcc3d3cb2c7578e1332bc155f
Luca <luca.cavagnoli@gmail.com>
2021-10-16 15:27:59 +0200

    list to detail

--- src/pages/products/ProductsComponent.tsx
+++ src/pages/products/ProductsComponent.tsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+
 const ProductsComponent = () => {
   return (
-    <h1>The Products Page</h1>
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Course</Link>
+        </li>
+      </ul>
+    </section>
   );
 };
 
