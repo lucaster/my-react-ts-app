9bef8ff31ae3f5be620509ad8021f1e7513b5924
Luca <luca.cavagnoli@gmail.com>
2021-10-16 11:16:24 +0200

    added 2 routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import NewTodoComponent from './components/NewTodoComponent';
 import TodosComponent from './components/TodosComponent';
+import ProductsComponent from './pages/products/ProductsComponent';
+import WelcomeComponent from './pages/welcome/WelcomeComponent';
 import ServicesContextProviderComponent from './store/services-context';
 import TodosContextProviderComponent from './store/todos-context';
 
 function App() {
   return (
-    <ServicesContextProviderComponent>
-      <TodosContextProviderComponent>
-        <NewTodoComponent />
-        <TodosComponent />
-      </TodosContextProviderComponent>
-    </ServicesContextProviderComponent>
+    <div>
+      <div>
+        <h1>Todos</h1>
+        <ServicesContextProviderComponent>
+          <TodosContextProviderComponent>
+            <NewTodoComponent />
+            <TodosComponent />
+          </TodosContextProviderComponent>
+        </ServicesContextProviderComponent>
+      </div>
+      <hr />
+      <div>
+        <h1>Routing:</h1>
+        <BrowserRouter>
+          <Route path="/welcome">
+            <WelcomeComponent />
+          </Route>
+          <Route path="/products">
+            <ProductsComponent />
+          </Route>
+        </BrowserRouter>
+      </div>
+    </div>
   );
 }
 
---
+++ src/pages/welcome/WelcomeComponent.tsx
@@ -0,0 +1,7 @@
+const WelcomeComponent = () => {
+  return (
+    <h1>The Welcome Page</h1>
+  );
+};
+
+export default WelcomeComponent;
