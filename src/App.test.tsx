ac14fe578c931c77cccec487a7d2508383be23e4
Luca <luca.cavagnoli@gmail.com>
2021-10-17 21:27:11 +0200

    describe it

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders App', () => {
-  render(<App />);
-  const elem = screen.getByText(/^Todos$/g);
-  expect(elem).toBeInTheDocument();
+describe('App', () => {
+  it('renders!', () => {
+    render(<App />);
+    const elem = screen.getByText(/^Todos$/g);
+    expect(elem).toBeInTheDocument();
+  });
+  test('renders', () => {
+    render(<App />);
+    const elem = screen.getByText(/^Todos$/g);
+    expect(elem).toBeInTheDocument();
+  });
 });
